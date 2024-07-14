a333da6e12a87b8e0e73254ebe2ff5d9bb1bde52
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-07-14 14:32:22 +0600

    section:13 PROJECT: Bankist Website

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -1,14 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+// Modal Elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,13 +17,17 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// Open Modal Event Listener
+btnsOpenModal.forEach((btnOpenModal) =>
+  btnOpenModal.addEventListener('click', openModal)
+);
 
+// Close Modal Event Listener
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
+// Close Modal Using 'Escape' key
+document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
