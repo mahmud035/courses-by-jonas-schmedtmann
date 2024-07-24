fdc50ed163c6c6861514905e334d191832cd9c57
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-07-24 08:56:27 +0600

    Uncomment code and add {} braces

--- complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -1,71 +1,78 @@
 'use strict';
 
-/* const cutFruitPieces = (fruit) => {
-  return 4 * fruit;
-};
+{
+  const cutFruitPieces = (fruit) => {
+    return 4 * fruit;
+  };
 
-const fruitProcessor = (apples, oranges) => {
-  const applePieces = cutFruitPieces(apples);
-  const juicePieces = cutFruitPieces(oranges);
+  const fruitProcessor = (apples, oranges) => {
+    const applePieces = cutFruitPieces(apples);
+    const juicePieces = cutFruitPieces(oranges);
 
-  const juice = `Juice with ${applePieces} pieces of apple and ${juicePieces} pieces of orange.`;
-  return juice;
-};
+    const juice = `Juice with ${applePieces} pieces of apple and ${juicePieces} pieces of orange.`;
+    return juice;
+  };
 
-console.log(fruitProcessor(2, 3)); */
-
-/* const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  age: 2037 - 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
-};
-
-console.log(
-  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`
-);
- */
-
-/* const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  birthYear: 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
-  hasDriversLicense: true,
-
-  calcAge: function () {
-    return 2037 - this.birthYear;
-  },
-
-  getSummary: function () {
-    const summary = `${
-      this.firstName
-    } is a ${this.calcAge()}-year old teacher, and he has ${
-      this?.hasDriversLicense ? 'a' : 'no'
-    } driver's license.`;
-
-    return summary;
-  },
-};
-
-console.log(jonas.calcAge());
-console.log(jonas.getSummary()); */
-
-/* const birthYears = [1991, 2007, 1969, 2020];
-const ages = [];
-
-const calcAge = (birthYear) => {
-  const now = new Date().getFullYear();
-  return now - birthYear;
-};
-
-for (let i = 0; i < birthYears.length; i++) {
-  ages[i] = calcAge(birthYears[i]);
+  // console.log(fruitProcessor(2, 3));
 }
 
-console.log('ages', ages); */
+{
+  const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+  };
+
+  // console.log(
+  //   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`
+  // );
+}
+
+{
+  const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function () {
+      return 2037 - this.birthYear;
+    },
+
+    getSummary: function () {
+      const summary = `${
+        this.firstName
+      } is a ${this.calcAge()}-year old teacher, and he has ${
+        this?.hasDriversLicense ? 'a' : 'no'
+      } driver's license.`;
+
+      return summary;
+    },
+  };
+
+  // console.log(jonas.calcAge());
+  // console.log(jonas.getSummary());
+}
+
+{
+  const birthYears = [1991, 2007, 1969, 2020];
+  const ages = [];
+
+  const calcAge = (birthYear) => {
+    const now = new Date().getFullYear();
+    return now - birthYear;
+  };
+
+  for (let i = 0; i < birthYears.length; i++) {
+    ages[i] = calcAge(birthYears[i]);
+  }
+
+  // console.log({ ages });
+}
 
 // const jonasArray = [
 //   'Jonas',
@@ -75,7 +82,7 @@ console.log('ages', ages); */
 //   ['Micheal', 'Peter', 'Steven'],
 // ];
 
-// // Looping Backwards
+// Looping Backwards
 // for (let i = jonasArray.length - 1; i >= 0; i--) {
 //   console.log(i, jonasArray[i]);
 // }
