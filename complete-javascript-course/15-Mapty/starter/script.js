6ab9af35ea3b833b937d904d61ca26a8f91b056d
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-07-28 21:35:52 +0600

    Important: Show Marker, Popup & Event using Laaflet Documentation

--- complete-javascript-course/15-Mapty/starter/script.js
+++ complete-javascript-course/15-Mapty/starter/script.js
@@ -31,6 +31,48 @@ if (navigator.geolocation) {
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu`
       );
+
+      const map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 25,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      //* Marker
+      // const marker = L.marker([latitude, longitude]).addTo(map);
+
+      //* Popup
+      // const popup = L.popup()
+      //   .setLatLng([latitude, longitude])
+      //   .setContent(
+      //     `<div class="leaflet-popup-content-wrapper"><div class="leaflet-popup-content" style="width: 148px;">🚴‍♀️ Cycling on July 28</div></div>`
+      //   )
+      //   .openOn(map);
+
+      //* Event, Popup & Marker Together
+      function onMapClick(e) {
+        // console.log(e);
+        const date = new Date();
+        const options = {
+          month: 'long',
+          day: '2-digit',
+        };
+        const time = new Intl.DateTimeFormat(
+          navigator.language,
+          options
+        ).format(date);
+
+        const Point = L.point(0, -27);
+        const marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+        const popup = L.popup({ offset: Point })
+          .setLatLng(e.latlng)
+          .setContent(`🏃‍♂️ Running on ${time} ${e.latlng.toString()}`)
+          .openOn(map);
+      }
+
+      map.on('click', onMapClick);
     },
 
     (error) => {
