132ac60a898a342c2930c15b9d83a32190c3f3de
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-07-30 23:44:06 +0600

    Promises and the Fetch API

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -79,36 +79,142 @@ const countriesContainer = document.querySelector('.countries');
 {
   // 1. First get the country data by fullName
   const getCountry = async (countryName) => {
-    const res = await fetch(
-      `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
-    );
-    const data = await res.json();
-    console.log(data[0]);
-    getNeighbourCountry(data[0]);
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+      );
+      const data = await res.json();
+      console.log(data[0]);
+      getNeighbourCountry(data[0]);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  getCountry('Bangladesh');
+  // getCountry('Bangladesh');
 
   // 2. And then get one neighbour country data
   const getNeighbourCountry = async (data) => {
-    const neighbourCountryCode = data?.borders?.[0]; // get first neighbour country code
+    try {
+      const neighbourCountryCode = data?.borders?.[0]; // get first neighbour country code
 
-    const res = await fetch(
-      `https://restcountries.com/v3.1/alpha/${neighbourCountryCode}`
-    );
-    const neighbourCountry = await res.json();
-    console.log(neighbourCountry[0]);
+      const res = await fetch(
+        `https://restcountries.com/v3.1/alpha/${neighbourCountryCode}`
+      );
+      const neighbourCountry = await res.json();
+      console.log(neighbourCountry[0]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  setTimeout(() => {
-    console.log('1 second passed');
-    setTimeout(() => {
-      console.log('2 seconds passed');
-      setTimeout(() => {
-        console.log('3 seconds passed');
-        setTimeout(() => {
-          console.log('4 seconds passed');
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
+  // WARNING: Don't do this. (Callback Hell)
+
+  // setTimeout(() => {
+  //   console.log('1 second passed');
+  //   setTimeout(() => {
+  //     console.log('2 seconds passed');
+  //     setTimeout(() => {
+  //       console.log('3 seconds passed');
+  //       setTimeout(() => {
+  //         console.log('4 seconds passed');
+  //       }, 1000);
+  //     }, 1000);
+  //   }, 1000);
+  // }, 1000);
+}
+
+//* Promises and the Fetch API
+
+{
+  //* Promise
+
+  // Creating a Promise
+  const myPromise = new Promise((resolve, reject) => {
+    // Asynchronous operation
+    let success = true;
+
+    if (success) {
+      resolve('Operation was successful');
+    } else {
+      reject('Operation failed');
+    }
+  });
+
+  // Consuming Promises or Using Promises
+  myPromise
+    .then((result) => {
+      console.log(result); // Operation was successful
+    })
+    .catch((error) => {
+      console.log(error); // Operation failed
+    })
+    .finally(() => {
+      console.log('Promise settled');
+    });
+
+  //* Fetch API
+
+  // Basic Fetch Request
+  fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network request was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(
+        `There was a problem with the fetch operation: ${error.message}`
+      );
+    });
+
+  // Advanced Fetch Options
+  fetch(`https://jsonplaceholder.typicode.com/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(
+        `There was a problem with the fetch operation: ${error.message}`
+      );
+    });
+
+  // TODO: NOTE: IMPORTANT: Combining Promises and Fetch API with
+  // Both Promises and Fetch API can be combined to handle network requests in a more readable and manageable way, especially with async/await.
+
+  const fetchPost = async () => {
+    try {
+      const url = `https://jsonplaceholder.typicode.com/posts/1`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(
+        `There was a problem with the fetch operation: ${error.message}`
+      );
+    }
+  };
+  fetchPost();
 }
