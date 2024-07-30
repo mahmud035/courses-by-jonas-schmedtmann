353721df54ebfd5afa4e12f7072ad061f424be32
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-07-30 13:13:19 +0600

    Display All Countries From Rest Countries API

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -4,5 +4,46 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+//* Display All Countries From Rest Countries API
+
 {
+  const loadCountries = async () => {
+    const res = await fetch(`https://restcountries.com/v3.1/all`);
+    const data = await res.json();
+    // console.log(data);
+    displayCountries(data);
+  };
+
+  const displayCountries = (countries) => {
+    countries.forEach((country) => {
+      const article = document.createElement('article');
+      article.classList.add('country');
+      article.innerHTML = `      
+          <img class="country__img" src=${country?.flags?.png} />
+          <div class="country__data">
+            <h3 class="country__name">${country?.name?.common}</h3>
+            <h4 class="country__region">${country?.region}</h4>
+            <p class="country__row"><span>👫</span>
+               ${country?.population}
+            </p>
+            <p class="country__row"><span>🗣️</span>
+               ${
+                 country?.languages
+                   ? Object.keys(country.languages).join(', ').toUpperCase()
+                   : 'N/A'
+               }
+            </p>
+            <p class="country__row"><span>💰</span>
+              ${
+                country.currencies
+                  ? Object.keys(country.currencies).join(', ')
+                  : 'N/A'
+              }
+            </p>
+          </div>`;
+      countriesContainer.appendChild(article);
+    });
+  };
+
+  loadCountries();
 }
