1197d5afd8baa838c780333053650a11cb03e924
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-08-16 15:33:01 +0600

    Rendering the Recipe

--- complete-javascript-course/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course/18-forkify/starter/src/js/controller.js
@@ -1,5 +1,7 @@
 // API url: https://forkify-api.herokuapp.com/v2
 
+import icons from 'url:../img/icons.svg';
+
 // Selecting elements
 const recipeContainer = document.querySelector('.recipe');
 
@@ -11,8 +13,23 @@ const timeout = (s) => {
   });
 };
 
+const renderSpinner = (parentEl) => {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async () => {
   try {
+    // 1. Loading recipe
+    renderSpinner(recipeContainer);
+
     const res = await fetch(
       `https://forkify-api.herokuapp.com/api/get?rId=46956`
     );
@@ -25,15 +42,110 @@ const showRecipe = async () => {
     recipe = {
       id: recipe.recipe_id,
       title: recipe.title,
+      image: recipe.image_url,
       publisher: recipe.publisher,
       ingredients: recipe.ingredients,
-      imageURL: recipe.image_url,
-      publisherURL: recipe.publisher_url,
-      sourceURL: recipe.source_url,
+      publisherUrl: recipe.publisher_url,
+      sourceUrl: recipe.source_url,
       socialRank: recipe.social_rank,
     };
 
+    const { id, title, image, publisher, ingredients, sourceUrl } =
+      recipe || {};
+
     console.log(recipe);
+
+    // 2. Rendering recipe
+    const markup = `
+        <figure class="recipe__fig">
+          <img src=${image} alt=${title} class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${title}</span>
+          </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">45</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">4</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="${icons}#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+            ${ingredients
+              .map((ingredient) => {
+                return `
+                <li class="recipe__ingredient">
+                  <svg class="recipe__icon">
+                    <use href="${icons}#icon-check"></use>
+                  </svg>
+                  <div class="recipe__description">
+                    ${ingredient}
+                  </div>
+                </li>`;
+              })
+              .join('')}
+          </ul>
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${publisher}</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href=${sourceUrl}
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>
+    `;
+
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (error) {
     console.error(error.message);
   }
