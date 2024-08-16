e77bede6077ea956784926340409ea63201bd366
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-08-16 14:48:18 +0600

    Loading a Recipe from API

--- complete-javascript-course/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course/18-forkify/starter/src/js/controller.js
@@ -1,11 +1,41 @@
-// Live link: https://forkify-api.herokuapp.com/v2
+// API url: https://forkify-api.herokuapp.com/v2
 
+// Selecting elements
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = (s) => {
   return new Promise((_, reject) => {
     setTimeout(() => {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
+      reject(new Error(`Request took too long! Timeout after ${s} seconds`));
     }, s * 1000);
   });
 };
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/get?rId=46956`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${res.status} Recipe ${res.statusText}`);
+
+    let { recipe } = data;
+
+    recipe = {
+      id: recipe.recipe_id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      ingredients: recipe.ingredients,
+      imageURL: recipe.image_url,
+      publisherURL: recipe.publisher_url,
+      sourceURL: recipe.source_url,
+      socialRank: recipe.social_rank,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+showRecipe();
