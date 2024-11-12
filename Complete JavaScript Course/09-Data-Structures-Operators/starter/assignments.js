c77f3936fa4a7b1080e2468def9e5c410d99844b
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-12 15:20:47 +0600

    JavaScript course assignment - Rest Pattern and Parameters

--- Complete JavaScript Course/09-Data-Structures-Operators/starter/assignments.js
+++ Complete JavaScript Course/09-Data-Structures-Operators/starter/assignments.js
@@ -313,6 +313,25 @@ const books = [
   const spellWord = (str) => console.log(str.split('').join(' '));
   const spellWord2 = (str) => console.log(...str);
 
-  spellWord('JavaScript');
-  spellWord2('JavaScript');
+  // spellWord('JavaScript');
+  // spellWord2('JavaScript');
+}
+
+//* Rest Pattern and Parameters
+{
+  // 4.1
+  const [mainKeyword, ...rest] = books[0].keywords;
+
+  // console.log(mainKeyword, rest);
+
+  // 4.2
+  const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+
+  // console.log(bookPublisher, restOfTheBook);
+
+  // 4.3
+  const printBookAuthorsCount = (title, ...authors) =>
+    console.log(`The book "${title}" has ${authors.length} authors`);
+
+  // printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
 }
